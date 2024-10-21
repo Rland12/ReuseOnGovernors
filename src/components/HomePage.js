9e2bbe66cb6acec611b82f395dac3aad71f2251b
BuildTools <unconfigured@null.spigotmc.org>
2024-10-21 00:06:50 -0400

    Updated code with new changes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import NavigationBar from './NavigationBar';
 import './styles/HomePageStyles.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import MarkerIcon from 'leaflet/dist/images/marker-icon.png';
 
-function home() {  
-    return (
-        <div className='main-content'>
-  
 
-            
-            <NavigationBar />
-        </div>
-      
-    );
-  }
-  
-  export default home;
+const customMarker = new L.Icon({
+  iconUrl: MarkerIcon,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+function Home() {  
+  return (
+    <div className='main-content'>
+      <div className='map-container'>
+        <MapContainer
+          center={[40.6886, -74.0189]}
+          zoom={15}
+          style={{ height: '300px', width: '100%' }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[40.6886, -74.0189]} icon={customMarker}>
+            <Popup>Governor's Island</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default Home;
+
+
--- src/components/VendorsPage.js
+++ src/components/VendorsPage.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import NavigationBar from './NavigationBar';
 import './styles/VendorsPageStyles.css';
-import Eva from './vendor-icons/little-evas-icon.png';
-import Joe from './vendor-icons/joecoffee_icon.jpg';
-import Taco from './vendor-icons/taco-vista-icon.jpg';
+import React from "react";
+import { FaLeaf, FaBreadSlice } from "react-icons/fa"; // Example icons for dietary restrictions
 
-function VendorsPage() {
+const Vendors = () => {
+  // data for vendors, including URLs for their sites
+  const vendors = [
+    {
+      id: 1,
+      name: "Little Eva's",
+      description:
+        "Little Eva's features classic grilled fare like hamburgers, brats, crispy codfish sandwiches, vegan spring rolls, homemade baked goods, and more, along with an extensive drink menu (both alcoholic & non-alcoholic)",
+      image:
+        "https://gov-island-site.s3.amazonaws.com/pages/_foodVendors2x/little-evas-fish-chips-cropped.png", // Placeholder for restaurant image
+      dietary: ["Vegan", "Gluten-Free"],
+      url: "https://www.littleevas.com/",
+    },
+    {
+      id: 2,
+      name: "Joe Coffee",
+      description:
+        "Roasting 100% of its own beans in Long Island City, Joe Coffee is New York's hometown roaster. Here on Governors Island, they're serving up their delicious coffee alongside teas, and baked treats. Make sure to stop by on your way over to the Island for seasonal lattes, Coldstretto cold brew espresso, breakfast tacos, and more.",
+      image:
+        "https://gov-island-site.s3.amazonaws.com/food/_foodVendors2x/joecoffee_Catering-Block2.jpg",
+      dietary: ["Gluten-Free"],
+      url: "https://joecoffeecompany.com/",
+    },
+    {
+      id: 3,
+      name: "Taco Vista",
+      description:
+        "Enjoy tasty tacos with in a scenic setting at the aptly named Taco Vista! If you’re thirsty, pair your tacos with a beer or an expertly mixed margarita, sit back, and enjoy the view.",
+      image:
+        "https://gov-island-site.s3.amazonaws.com/pages/_foodVendors2x/taco-vista-cropped.jpg",
+      dietary: ["Organic", "Gluten-Free"],
+      url: "https://www.tacovista.co/",
+    },
+  ];
+
   return (
-    <div className='main-content'>
-      <div className='header'>
-        <h1 className='heading'>VENDORS</h1>
-      </div>
+    <div className="vendors-container">
+      <header className="vendors-header">
+        <h2>Vendors</h2>
+      </header>
+      {vendors.map((vendor) => (
+        <div key={vendor.id} className="vendor-card">
+          {/* Restaurant Image */}
+          <img src={vendor.image} alt={vendor.name} className="vendor-image" />
 
-      <div className='vendor-container'>
-        <img className='eva-icon' src={Eva} alt= 'EvaIcon'></img>
-        <h2 className='name2'>Little Eva's</h2>
-        <h3 className='bio'>Gluten-Free</h3>
+          {/* Vendor Info */}
+          <h3>{vendor.name}</h3>
+          <p>{vendor.description}</p>
 
-      </div>
-      <NavigationBar />
+          {/* Dietary Restriction Icons */}
+          <div className="dietary-icons">
+            {vendor.dietary.includes("Vegan") && (
+              <FaLeaf className="dietary-icon" title="Vegan" />
+            )}
+            {vendor.dietary.includes("Gluten-Free") && (
+              <FaBreadSlice className="dietary-icon" title="Gluten-Free" />
+            )}
+            {/* Add more icons for other dietary restrictions */}
+          </div>
+
+          {/* View Menu Button with Link */}
+          <a href={vendor.url} target="_blank" rel="noopener noreferrer">
+            <button className="view-menu-button">View Menu</button>
+          </a>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default VendorsPage; 
+export default Vendors;
